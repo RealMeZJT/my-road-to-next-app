2dcad9c8671faafe170e9b4ba65343691302f536
ZhouJiatao <709855120@qq.com>
2025-08-17 21:39:48 +0800

    lesson 41

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
-import {ticketsPath} from "@/paths";
+import { ticketsPath } from "@/paths";
 const HomePage = () => {
   return (
-    <div>
-  <h2 className="text-lg">Home Page</h2>
-  <Link href={ticketsPath()} className="underline">Go To Ticket</Link>
-  </div>
-);
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div >
+        <h2 className="text-3xl font-bold tracking-tight">HomePage</h2>
+        <p className="text-sm text-muted-foreground">Your home place to start</p>
+      </div>
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="underline">
+          Go To Ticket
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,24 +1,30 @@
 
 import Link from "next/link";
-import {initialTickets} from "../../data";
-import {ticketPath} from "@/paths";
+import { initialTickets } from "../../data";
+import { ticketPath } from "@/paths";
 const TicketsPage = () => {
     return (
-        <div>
-            {initialTickets.map((ticket) => (
-                <div key={ticket.id}>
-                    <h2 className="text-lg">{ticket.title}</h2>
+        <div className="flex-1 flex flex-col gap-y-8">
+            <div>
+                <h2 className="text-3xl font-bold tracking-tight">TicketsPage</h2>
+                <p className="text-sm text-muted-foreground">All your tickets at one place</p>
+            </div>
+            <div className="flex-1 flex flex-col items-center gap-y-4">
+                {initialTickets.map((ticket) => (
+                    <div key={ticket.id} className="w-full max-w-[420px] border border-slate-100 rounded">
+                        <h2 className="text-lg font-semibold truncate">{ticket.title}</h2>
+                        <p className="text-sm text-slate-500 truncate">{ticket.content}</p>
 
-                    <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                    View
-                    </Link>
-                </div>
-
-            ))}
+                        <Link href={ticketPath(ticket.id)} className="text-sm underline">
+                            View
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </div>
     );
-    
-    
+
+
 };
 
 export default TicketsPage;
